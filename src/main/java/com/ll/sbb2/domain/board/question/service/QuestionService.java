7b92c75b54f2f61b43036108afc04c70ffee87aa
solji <solji.choi89@gmail.com>
2024-12-26 23:29:18 +0900

    create detail page

--- src/main/java/com/ll/sbb2/domain/board/question/service/QuestionService.java
+++ src/main/java/com/ll/sbb2/domain/board/question/service/QuestionService.java
@@ -2,10 +2,12 @@ package com.ll.sbb2.domain.board.question.service;
 
 import com.ll.sbb2.domain.board.question.entity.Question;
 import com.ll.sbb2.domain.board.question.repository.QuestionRepository;
+import com.ll.sbb2.global.exceptions.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,5 +16,15 @@ public class QuestionService {
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+
+        if(question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
