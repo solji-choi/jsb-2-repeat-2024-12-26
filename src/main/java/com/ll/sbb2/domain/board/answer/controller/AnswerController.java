47ce15ef5f149ece8c35fe5a8691e924b2c9d145
solji <solji.choi89@gmail.com>
2024-12-27 01:19:48 +0900

    create question, 1st fin

--- src/main/java/com/ll/sbb2/domain/board/answer/controller/AnswerController.java
+++ src/main/java/com/ll/sbb2/domain/board/answer/controller/AnswerController.java
@@ -1,11 +1,14 @@
 package com.ll.sbb2.domain.board.answer.controller;
 
+import com.ll.sbb2.domain.board.answer.form.AnswerForm;
 import com.ll.sbb2.domain.board.answer.service.AnswerService;
 import com.ll.sbb2.domain.board.question.entity.Question;
 import com.ll.sbb2.domain.board.question.service.QuestionService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,12 +23,19 @@ public class AnswerController {
     @PostMapping("/create/{id}")
     public String createAnswer(
             Model model,
-            @PathVariable Integer id,
-            String content
+            @Valid AnswerForm answerForm,
+            BindingResult bindingResult,
+            @PathVariable Integer id
     ) {
         Question question = this.questionService.getQuestion(id);
 
-        this.answerService.create(question, content);
+        if(bindingResult.hasErrors()) {
+            model.addAttribute("question", question);
+
+            return "/domain/board/question/question_detail";
+        }
+
+        this.answerService.create(question, answerForm.getContent());
 
         return "redirect:/question/detail/%s".formatted(id);
     }
--- src/main/java/com/ll/sbb2/domain/board/question/controller/QuestionController.java
+++ src/main/java/com/ll/sbb2/domain/board/question/controller/QuestionController.java
@@ -1,12 +1,18 @@
 package com.ll.sbb2.domain.board.question.controller;
 
+import com.ll.sbb2.domain.board.answer.form.AnswerForm;
 import com.ll.sbb2.domain.board.question.entity.Question;
 import com.ll.sbb2.domain.board.question.service.QuestionService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -26,15 +32,49 @@ public class QuestionController {
         return "/domain/board/question/question_list";
     }
 
+
     @GetMapping("/detail/{id}")
     public String detail(
             Model model,
-            @PathVariable Integer id
+            @PathVariable Integer id,
+            AnswerForm answerform
     ) {
         Question question = this.questionService.getQuestion(id);
 
         model.addAttribute("question", question);
 
         return "/domain/board/question/question_detail";
+    }
+
+
+    record QuestionForm (
+            @NotEmpty(message = "제목은 필수항목입니다.")
+            @Size(max = 200)
+            String subject,
+
+            @NotEmpty(message = "내용은 필수항목입니다.")
+            @Size(max = 200)
+            String content
+    ) {}
+
+    @GetMapping("/create")
+    public String questionCreate(QuestionForm questionForm) {
+        return "/domain/board/question/question_form";
+    }
+
+    @PostMapping("/create")
+    public String questionCreate(
+            @Valid QuestionForm questionForm,
+            BindingResult bindingResult,
+            String subject,
+            String content
+    ) {
+        if(bindingResult.hasErrors()) {
+            return "/domain/board/question/question_form";
+        }
+
+        this.questionService.create(questionForm.subject, questionForm.content);
+
+        return "redirect:/question/list";
     }
 }
