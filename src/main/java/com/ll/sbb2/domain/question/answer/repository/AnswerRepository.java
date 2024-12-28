4e7d5a3684b8ce7eb9e30e134d740a06f46bc157
solji <solji.choi89@gmail.com>
2024-12-29 00:25:57 +0900

    reset

--- src/main/java/com/ll/sbb2/domain/question/answer/repository/AnswerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ll.sbb2.domain.question.answer.repository;
-
-import com.ll.sbb2.domain.question.answer.entity.Answer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnswerRepository extends JpaRepository<Answer, Integer> {
-}
