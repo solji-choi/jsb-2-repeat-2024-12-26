c7ded3d881ac87a4b0122ce048ab415ed8f7a172
solji <solji.choi89@gmail.com>
2024-12-29 01:38:29 +0900

    ~2-09

---
+++ src/main/java/com/ll/sbb2/domain/question/question/service/QuestionService.java
@@ -0,0 +1,18 @@
+package com.ll.sbb2.domain.question.question.service;
+
+import com.ll.sbb2.domain.question.question.entity.Question;
+import com.ll.sbb2.domain.question.question.repository.QuestionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class QuestionService {
+    private final QuestionRepository questionRepository;
+
+    public List<Question> findAll() {
+        return this.questionRepository.findAll();
+    }
+}
