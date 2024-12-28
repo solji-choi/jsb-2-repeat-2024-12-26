97e7b6fb205c7a64fb825f4d28ea6b6ffc86e8bb
solji <solji.choi89@gmail.com>
2024-12-28 22:52:28 +0900

    ~2-12

--- src/main/java/com/ll/sbb2/domain/question/question/controller/QuestionController.java
+++ src/main/java/com/ll/sbb2/domain/question/question/controller/QuestionController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -23,5 +24,17 @@ public class QuestionController {
         model.addAttribute("questionList", questionList);
 
         return "/domain/question/question/question_list";
+    }
+
+    @GetMapping("/detail/{id}")
+    public String detail(
+            Model model,
+            @PathVariable Integer id
+    ) {
+        Question question = this.questionService.findById(id);
+
+        model.addAttribute("question", question);
+
+        return "/domain/question/question/question_detail";
     }
 }
