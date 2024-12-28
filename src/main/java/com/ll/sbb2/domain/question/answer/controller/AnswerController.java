5d272214f38e5faed20704816aec8c134ed21a04
solji <solji.choi89@gmail.com>
2024-12-29 01:57:23 +0900

    ~2-12

---
+++ src/main/java/com/ll/sbb2/domain/question/answer/controller/AnswerController.java
@@ -0,0 +1,30 @@
+package com.ll.sbb2.domain.question.answer.controller;
+
+import com.ll.sbb2.domain.question.answer.service.AnswerService;
+import com.ll.sbb2.domain.question.question.entity.Question;
+import com.ll.sbb2.domain.question.question.service.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/answer")
+public class AnswerController {
+    private final AnswerService answerService;
+    private final QuestionService questionService;
+
+    @PostMapping("/create/{questionId}")
+    public String create(
+            @PathVariable Integer questionId,
+            String content
+    ) {
+        Question question = this.questionService.findById(questionId);
+
+        this.answerService.create(question, content);
+
+        return "redirect:/question/detail/%d".formatted(questionId);
+    }
+}
--- src/main/java/com/ll/sbb2/domain/question/question/service/QuestionService.java
+++ src/main/java/com/ll/sbb2/domain/question/question/service/QuestionService.java
@@ -2,10 +2,12 @@ package com.ll.sbb2.domain.question.question.service;
 
 import com.ll.sbb2.domain.question.question.entity.Question;
 import com.ll.sbb2.domain.question.question.repository.QuestionRepository;
+import com.ll.sbb2.global.exceptions.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,5 +16,15 @@ public class QuestionService {
 
     public List<Question> findAll() {
         return this.questionRepository.findAll();
+    }
+
+    public Question findById(Integer id) {
+        Optional<Question> opQuestion = this.questionRepository.findById(id);
+
+        if(opQuestion.isPresent()) {
+            return opQuestion.get();
+        } else {
+            throw new DataNotFoundException("존재하지 않는 id입니다.");
+        }
     }
 }
